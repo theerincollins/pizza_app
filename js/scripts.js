d0893fa029a1ff5846e448d8f3b2eff8c4088da6
Erin <erincollinsthethird@gmail.com>
2015-06-05 11:07:13 -0700

    include functionality to add pizzas to order and to calcualate total for order

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,43 @@ Pizza.prototype.selectSize = function(size) {
 Pizza.prototype.selectQuantity = function(quantity) {
   this.quantity = quantity;
 }
+
+var pizzaCost = function(pizza) {
+  var toppingsCost = 0;
+  var toppingsHash = pizza.toppings;
+  for (var topping in toppingsHash) {
+    toppingsCost = toppingsCost + toppingsHash[topping]
+  }
+
+  var sizeCost = 0;
+  var sizeHash = pizza.size;
+  for (var size in sizeHash) {
+    sizeCost = sizeCost + sizeHash[size]
+  }
+
+  return (sizeCost + toppingsCost) * pizza.quantity;
+
+}
+
+function Order(items, totalCost) {
+  this.items = items;
+  this.totalCost = totalCost;
+}
+
+Order.prototype.addToOrder = function(pizza) {
+  if (this.items === undefined) {
+    this.items = [];
+    this.items.push(pizza);
+  } else {
+    this.items.push(pizza);
+  }
+}
+
+Order.prototype.calculateTotal = function() {
+  var totalCost = 0;
+  var itemArray = this.items;
+  itemArray.forEach(function(pizza) {
+    totalCost = totalCost + pizzaCost(pizza);
+  });
+  this.totalCost = totalCost;
+};
--- spec/specs.js
+++ spec/specs.js
@@ -7,7 +7,7 @@ describe('Pizza', function() {
   });
 
   it("will return the size of a pizza", function() {
-    var testSize = {"x-large": 6000, "large": 5000, "medium": 4000, "small": 3000, "x-small": 2000};
+    var testSize = {"x-large": 6000};
     var testPizza = new Pizza();
     testPizza.selectSize(testSize);
     expect(testPizza.size).to.eql(testSize);
@@ -19,10 +19,47 @@ describe('Pizza', function() {
     testPizza.selectQuantity(testQuantity);
     expect(testPizza.quantity).to.eql(testQuantity);
   });
+
+  it("will return the cost of one type of pizza including with a quantity more than one", function() {
+    var testToppings = {"pepperoni": 5, "gold": 20, "ice cream": 5};
+    var testSize = {"large": 5000}
+    var testQuantity = 3;
+    var testPizza = new Pizza(testToppings, testSize, testQuantity)
+    expect(pizzaCost(testPizza)).to.equal(15090)
+  });
 });
 
 describe('Order', function() {
-  it("will return the cost of one type of pizza in an order", function() {
+  it("will return items and costs of all pizzas in an order", function() {
+    var testOrder = new Order ()
+    var testToppings = {"pepperoni": 5.00, "gold": 20.00, "ice cream": 5.00};
+    var testSize = {"large": 5000}
+    var testQuantity = 3;
+    var testPizza1 = new Pizza(testToppings, testSize, testQuantity)
 
+    var testToppings = {"sausage": 7.00, "gold": 20.00, "ice": 8.00};
+    var testSize = {"x-small": 2000}
+    var testQuantity = 3;
+    var testPizza2 = new Pizza(testToppings, testSize, testQuantity)
+    testOrder.addToOrder(testPizza1)
+    testOrder.addToOrder(testPizza2)
+    expect(testOrder.items).to.eql([testPizza1, testPizza2])
+  });
+
+  it("will return items and costs of all pizzas in an order", function() {
+    var testOrder = new Order ()
+    var testToppings = {"pepperoni": 5.00, "gold": 20.00, "ice cream": 5.00};
+    var testSize = {"large": 5000}
+    var testQuantity = 3;
+    var testPizza1 = new Pizza(testToppings, testSize, testQuantity)
+
+    var testToppings = {"sausage": 7.00, "gold": 20.00, "ice": 8.00};
+    var testSize = {"x-small": 2000}
+    var testQuantity = 3;
+    var testPizza2 = new Pizza(testToppings, testSize, testQuantity)
+    testOrder.addToOrder(testPizza1);
+    testOrder.addToOrder(testPizza2);
+    testOrder.calculateTotal();
+    expect(testOrder.totalCost).to.eql(21195)
   });
 });
