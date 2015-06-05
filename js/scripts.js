6977962c2a3a01cf23e9908f6b83f62686fc48bd
Erin <erincollinsthethird@gmail.com>
2015-06-05 12:29:56 -0700

    A user can select toppings, size and quantity

--- js/scripts.js
+++ js/scripts.js
@@ -55,3 +55,71 @@ Order.prototype.calculateTotal = function() {
   });
   this.totalCost = totalCost;
 };
+
+$(document).ready(function() {
+
+  $("form#toppingsForm").submit(function() {
+    event.preventDefault();
+    var pizzaToppings = [];
+    $(':checkbox:checked').each(function(i){
+      pizzaToppings[i] = parseInt($(this).val());
+    });
+
+// I am realizeing now that using a hash for the toppings may have not been the
+// best way to go, but due to time contstraints, I'm going to keep going with it.
+// The following code is to make the pizza topping prices back into a hash. I
+// couldn't quite figure out how to do it from the form inputs.
+    var pizzaToppingsHash = {};
+    pizzaToppings.forEach(function(toppingPrice) {
+      if (toppingPrice === 1) {
+        pizzaToppingsHash["Sausage"] = 1
+      } else if (toppingPrice === 15) {
+        pizzaToppingsHash["Brownies"] = 15
+      } else if (toppingPrice === 2) {
+        pizzaToppingsHash["Liquid Gold"] = 2
+      } else if (toppingPrice === 20) {
+        pizzaToppingsHash["Pepperoni"] = 20
+      } else if (toppingPrice === 12) {
+        pizzaToppingsHash["Goat Cheese"] = 12
+      } else if (toppingPrice === 3) {
+        pizzaToppingsHash["Basil"] = 3
+      } else if (toppingPrice === 199) {
+        pizzaToppingsHash["Diamond Crusted Shrimp"] = 199
+      } else if (toppingPrice === 200) {
+        pizzaToppingsHash["Platinum Crusted Mushrooms"] = 200
+      } else if (toppingPrice === 8) {
+        pizzaToppingsHash["Onion"] = 8
+      } else if (toppingPrice === 9) {
+        pizzaToppingsHash["Candied Apples"] = 9
+      }
+    });
+  // });
+
+  // $("form#size-form").submit(function() {
+    // event.preventDefault();
+    var pizzaSizeHash = {};
+    var pizzaSize = parseInt($("#size-select").val());
+    if (pizzaSize === 50) {
+      pizzaSizeHash["Itty Bitty"] = 50
+    } else if (pizzaSize === 100) {
+      pizzaSizeHash["Small"] = 100
+    } else if (pizzaSize === 200) {
+      pizzaSizeHash["Medium"] = 200
+    } else if (pizzaSize === 400) {
+      pizzaSizeHash["Large"] = 400
+    } else if (pizzaSize === 800) {
+      pizzaSizeHash["X-Large"] = 800
+    } else if (pizzaSize === 1600) {
+      pizzaSizeHash["Super Gigantic"] = 1600
+    }
+  // });
+
+  // $("form#quantity-form").submit(function() {
+  //   event.preventDefault();
+    var quantity = parseInt($("#quantity-input").val());
+
+    console.log(quantity, pizzaToppingsHash, pizzaSizeHash)
+  });
+
+
+});
